09fce445c898b8c5772c1635d9bac31573f8b766
Adam <01187561@pw.edu.pl>
2023-12-21 21:59:25 +0100

    implementacja metody eliminacji Gaussa

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -1,5 +1,5 @@
 #include "gauss.h"
-
+#include <stdio.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -8,7 +8,33 @@ int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
 		 */
+	
 
+
+
+	for(int k = 0; k<mat->r; k++)
+	{
+		double el_gl = mat->data[k][k];
+		if(el_gl==0)
+		{
+			fprintf(stderr, "macierz osobliwa - dzielenie przez 0");
+			return 1;
+		}
+		for(int i = 1+k; i<mat->r; i++)
+		{
+			double el = mat->data[i][k];
+			for(int j = 0+k; j<mat->c; j++)
+			{
+			
+				mat->data[i][j] -= (mat->data[k][j]*(el/el_gl));
+				
+		
+			}
+			b->data[i][0] -= (b->data[k][0]*(el/el_gl));
+
+
+		}
+	}
 		return 0;
 }
 
